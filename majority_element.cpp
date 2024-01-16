a4ee242f74e4ca5015227062042cc70cd2f21ff8
Scott Gordon <39602477+SGordon13@users.noreply.github.com>
2024-01-16 02:31:50 -0500

    Create majority_element.cpp

---
+++ majority_element.cpp
@@ -0,0 +1,43 @@
+/*
+PROBLEM DESCRIPTION
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+*/
+
+/*
+SOLUTION DESCRIPTION
+incrememnt a map value for each element in nums.
+loop through the map and take the key with the largest value.
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> myMap;
+        for(int i = 0; i < nums.size(); i++){
+            myMap[nums[i]]++;
+        }
+
+        int maxKey = nums[0];
+        int maxVal = 0;
+        for(auto elem: myMap){
+            if(elem.second > maxVal){
+                maxKey = elem.first;
+                maxVal = elem.second;
+            }
+        }
+        return maxKey;
+    }
+};
