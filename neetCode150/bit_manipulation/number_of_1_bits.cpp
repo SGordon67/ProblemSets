eab64d4f3cbe69dac4a84928150d86b59aad5c39
Scott Gordon <39602477+SGordon67@users.noreply.github.com>
2024-05-07 22:55:07 -0400

    Create number_of_1_bits.cpp

---
+++ neetCode150/bit_manipulation/number_of_1_bits.cpp
@@ -0,0 +1,44 @@
+/*
+PROBLEM (easy)
+Write a function that takes the binary representation of a positive integer and returns the number of 
+set bits it has (also known as the Hamming weight).
+
+Example 1:
+Input: n = 11
+Output: 3
+Explanation:
+The input binary string 1011 has a total of three set bits.
+
+Example 2:
+Input: n = 128
+Output: 1
+Explanation:
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+Input: n = 2147483645
+Output: 30
+Explanation:
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
+Constraints:
+1 <= n <= 231 - 1
+
+Follow up: If this function is called many times, how would you optimize it?
+*/
+/*
+SOLUTION
+Divide n by 2 until n = 0;
+Add a value to set if n%2 is 1, to account for remainders/set bits.
+*/
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int set = 0;
+        while(n > 0){
+            if(n%2)set++;
+            n/=2;
+        }
+        return set;
+    }
+};
