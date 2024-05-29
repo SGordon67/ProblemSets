b8baa0bc2718b413b06ff0412794da535181e463
Scott Gordon <39602477+SGordon67@users.noreply.github.com>
2024-05-28 22:46:37 -0400

    Create merge_nodes_in_between_zeros.cpp

---
+++ medium/merge_nodes_in_between_zeros.cpp
@@ -0,0 +1,66 @@
+/*
+PROBLEM (medium)
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+Return the head of the modified linked list.
+
+Example 1:
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
+Example 2:
+Input: head = [0,1,0,3,0,2,2,0]
+Output: [1,3,4]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 1 = 1.
+- The sum of the nodes marked in red: 3 = 3.
+- The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+Constraints:
+The number of nodes in the list is in the range [3, 2 * 105].
+0 <= Node.val <= 1000
+There are no two consecutive nodes with Node.val == 0.
+The beginning and end of the linked list have Node.val == 0.
+*/
+/*
+SOLUTION
+Go through the linked list, summing each value until you reach a zero or the end of the list.
+Add that sum to a new linked list and repeat the process adding to the resulting linked list for each group of numbers.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+
+        ListNode* ptr = head;
+
+        ListNode* dummy = new ListNode();
+        ListNode* dptr = dummy;
+
+        while(ptr->next != nullptr){
+            int curSum = 0;
+            ptr = ptr->next;
+            while(ptr != nullptr && ptr->val != 0){
+                curSum += ptr->val;
+                ptr = ptr->next;
+            }
+            dptr->next = new ListNode(curSum);
+            dptr = dptr->next;
+        }
+        return dummy->next;
+    }
+};
