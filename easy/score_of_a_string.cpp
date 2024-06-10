4f973e5c356e9bbdca04a4e6ef1551cfcc479efb
Scott Gordon <39602477+SGordon67@users.noreply.github.com>
2024-06-09 21:57:22 -0400

    Create score_of_a_string.cpp

---
+++ easy/score_of_a_string.cpp
@@ -0,0 +1,34 @@
+/*
+PROBLEM (easy)
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+Return the score of s.
+
+Example 1:
+Input: s = "hello"
+Output: 13
+Explanation:
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+Input: s = "zaz"
+Output: 50
+Explanation:
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+Constraints:
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+*/
+class Solution {
+public:
+    int scoreOfString(string s) {
+        // running total
+        int total = 0;
+        for(int i = 0; i < s.length()-1; i++){
+            // add absolute value of adjacent difference to running total
+            total += abs(s[i] - s[i+1]);
+        }
+        // return sum
+        return total;
+    }
+};
