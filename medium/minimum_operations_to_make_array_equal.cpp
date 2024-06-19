01ecb1bf2169fe48726236e60edf115c054bb954
Scott Gordon <39602477+SGordon67@users.noreply.github.com>
2024-06-19 15:09:33 -0400

    Create minimum_operations_to_make_array_equal.cpp

---
+++ medium/minimum_operations_to_make_array_equal.cpp
@@ -0,0 +1,45 @@
+/*
+PROBLEM (medium)
+You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e., 0 <= i < n).
+
+In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e., perform arr[x] -=1 and arr[y] += 1). 
+The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
+
+Given an integer n, the length of the array, return the minimum number of operations needed to make all the elements of arr equal.
+
+Example 1:
+Input: n = 3
+Output: 2
+Explanation: arr = [1, 3, 5]
+First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
+
+Example 2:
+Input: n = 6
+Output: 9
+
+Constraints:
+1 <= n <= 104
+*/
+class Solution {
+public:
+    int minOperations(int n) {
+        // get the median, since all numbers will go to median
+        int median = 0;
+        if(n%2){
+            median = n+1;
+        } else{
+            cout << (((n+1) + (n))/2);
+            median = (((n+1) + (n))/2);
+        }
+        
+        // get the number of total operations we must do and sum them up
+        int operations = 0;
+        for(int i = 0; i < n; i++){
+            operations  += abs(median - ((i*2)+1));
+        }
+
+        // since we can do two operations at once, divide by 2 and return
+        return operations / 2;
+    }
+};
